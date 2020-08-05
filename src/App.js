39f37c41a51987db8569b4fb04453891120d4803
Jesper Pedersen <pejes@aarhus.dk>
2020-08-05 13:03:58 +0200

    Add iOS install banner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import NavBar from './components/navBar'
+import AppInstallBanner from './components/appInstallBanner'
 import { Route, Redirect, Switch } from 'react-router-dom'
 import EventComponent from './components/event'
 import Conference from './components/conference'
@@ -109,6 +110,14 @@ function App (props) {
       )
   }, [])
 
+  // Detects if device is on iOS
+  const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase()
+    return /iphone|ipad|ipod/.test(userAgent)
+  }
+  // Detects if device is in standalone mode
+  const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone)
+
   return (
     <>
       {!loading && !error &&
@@ -121,6 +130,8 @@ function App (props) {
               <Redirect from='/' to='/konference' />
             </Switch>
           </Container>
+          {isIos() && !isInStandaloneMode() &&
+            <AppInstallBanner />}
         </AppStateContext.Provider>}
       {loading &&
         <FontAwesomeIcon
