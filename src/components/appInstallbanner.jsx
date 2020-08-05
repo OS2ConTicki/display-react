acbeb6c39a97aaf4a8675931ca327561ad40a1a7
Jesper Pedersen <pejes@aarhus.dk>
2020-08-05 13:04:37 +0200

    Add appicon to main menu. Fix menu on iOS.

--- src/components/appInstallbanner.jsx
+++ src/components/appInstallbanner.jsx
@@ -6,7 +6,7 @@ const AppInstallBanner = () => {
   return (
     <Toast>
       <Toast.Header>
-        <img src='/icons/favicon-16x16.png' alt=''className='mr-1' />
+        <img src='/icons/favicon-16x16.png' alt='' className='mr-1' />
         <strong className='mr-auto'>Install Conference App</strong>
       </Toast.Header>
       <Toast.Body>Install this webapp on your iPhone: tab <img src={NavigationActionIcon} alt='iOS action menu' className='mx-3' /> and then Add to homescreen.</Toast.Body>
