7c896ac9a48578c725c9c151a04ab237a3877ccf
Jesper Pedersen <pejes@aarhus.dk>
2020-08-20 11:03:00 +0200

    Location styling. Link to location from event

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -8,10 +8,11 @@ import { format } from 'date-fns'
 import OrganizerList from '../organizer/OrganizerList'
 import SpeakerList from '../speaker/SpeakerList'
 import SponsorList from '../sponsor/SponsorList'
+import { Link } from 'react-router-dom'
 
 const Event = ({ event, onLike }) => {
   // Unwrap the event object.
-  const { title, image, description, ticketUrl, themes } = event
+  const { title, image, description, ticketUrl, themes, location } = event
 
   return (
     <div className='event'>
@@ -39,7 +40,7 @@ const Event = ({ event, onLike }) => {
         <Container>
           <Row>
             <Col md={4}><p className='lead mb-0'><strong className='mr-3'>Hvornår</strong>{format(new Date(event.start_time), 'eeee kk:mm')} to {format(new Date(event.end_time), 'kk:mm')}</p></Col>
-            <Col md={4}><p className='lead mb-0'><strong className='mr-3'>Hvor</strong>{event.location.title}</p></Col>
+            <Col md={4}><p className='lead mb-0'><strong className='mr-3'>Hvor</strong><Link to={`/location/${location.id}`} className='image-square'>{location.title}</Link></p></Col>
             <Col md={4}>
               <p className='lead mb-0'><strong className='mr-3'>Tema</strong>
                 {[themes].map(theme => (
