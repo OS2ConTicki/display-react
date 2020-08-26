80f3e8175ffd879c3f27ca551c1323d529bb890f
Mikkel Ricky <rimi@aarhus.dk>
2020-08-26 11:04:58 +0200

    JOBCAMP-61: Applied coding standards

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-var Encore = require("@symfony/webpack-encore");
+var Encore = require('@symfony/webpack-encore')
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
 }
 
 Encore
   // directory where compiled assets will be stored
-  .setOutputPath("public/dist/")
+  .setOutputPath('public/dist/')
   // public path used by the web server to access the output path
-  .setPublicPath("/dist")
+  .setPublicPath('/dist')
   // only needed for CDN's or sub-directory deploy
-  // .setManifestKeyPrefix('build/')
+  .setManifestKeyPrefix('dist/')
 
   /*
    * ENTRY CONFIG
@@ -23,10 +23,7 @@ Encore
    * Each entry will result in one JavaScript file (e.g. app.js)
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
-  .addEntry("entry", "./src/entry.js")
-
-  // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-  // .splitEntryChunks()
+  .addEntry('entry', './src/entry.js')
 
   // will require an extra script tag for runtime.js
   // but, you probably want this, unless you're building a single-page app
@@ -47,39 +44,28 @@ Encore
 
   // enables @babel/preset-env polyfills
   .configureBabelPresetEnv((config) => {
-    config.useBuiltIns = "usage";
-    config.corejs = 3;
+    config.useBuiltIns = 'usage'
+    config.corejs = 3
   })
 
   .configureBabel(function (babelConfig) {
-    babelConfig.plugins.push("@babel/plugin-proposal-class-properties");
+    babelConfig.plugins.push('@babel/plugin-proposal-class-properties')
   })
 
   // enables Sass/SCSS support
   .enableSassLoader()
 
-  // uncomment if you use TypeScript
-  // .enableTypeScriptLoader()
-
-  // uncomment to get integrity="..." attributes on your script & link tags
-  // requires WebpackEncoreBundle 1.4 or higher
-  // .enableIntegrityHashes(Encore.isProduction())
-
-  // uncomment if you're having problems with a jQuery plugin
-  .autoProvidejQuery()
-
   // uncomment if you use API Platform Admin (composer req api-admin)
   .enableReactPreset()
-// .addEntry('admin', './assets/js/admin.js')
 
   // Enable copy of static assets
   .copyFiles({
     from: './src/images',
     // if versioning is enabled, add the file hash too
-    //to: 'images/[path][name].[hash:8].[ext]',
+    // to: 'images/[path][name].[hash:8].[ext]',
     to: 'images/[path][name].[ext]',
     // only copy files matching this pattern
     pattern: /\.(png|jpg|jpeg)$/
   })
 
-module.exports = Encore.getWebpackConfig();
+module.exports = Encore.getWebpackConfig()
