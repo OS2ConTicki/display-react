a99cced6807162436386e380c86493083e2af866
Jesper Pedersen <pejes@aarhus.dk>
2020-09-03 11:08:28 +0200

    Change link. Add translation. Fix review comments.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,6 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Nav from 'react-bootstrap/Nav'
 import { useTranslate } from 'react-translate'
-import { Link } from 'react-router-dom'
 
 function Footer () {
   const t = useTranslate('Conticki')
@@ -22,8 +21,8 @@ function Footer () {
                 <p className='text-light mt-2'>{t('OS2Contiki - Conference and ticket management')}</p>
               </Col>
               <Col md={4}>
-                {conference.app.accessibility_statement.url &&
-                  <p className='text-light mt-2'><Link to={conference.app.accessibility_statement.url}>{conference.app.accessibility_statement.text}</Link></p>}
+                {conference.app.accessibility_statement?.url &&
+                  <p className='mt-2'><a className='text-light' href={conference.app.accessibility_statement.url}>{conference.app.accessibility_statement.text ? conference.app.accessibility_statement.text : t('Accessibility statement')}</a></p>}
               </Col>
               <Col md={4}>
                 <Nav variant='pills' defaultActiveKey='/home'>
