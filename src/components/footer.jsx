8f840ac3cf48306277c179aee4cd97f6b803c154
Jesper Pedersen <pejes@aarhus.dk>
2020-09-03 09:07:21 +0200

    Add link to accessibility_statement

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,10 +5,12 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Nav from 'react-bootstrap/Nav'
 import { useTranslate } from 'react-translate'
+import { Link } from 'react-router-dom'
 
 function Footer () {
   const t = useTranslate('Conticki')
   const context = useContext(AppStateContext)
+  const conference = context.conference.get
 
   return (
     <Container fluid className='bg-secondary'>
@@ -16,10 +18,14 @@ function Footer () {
         <Col>
           <Container>
             <Row className='my-3'>
-              <Col md={6} className=''>
+              <Col md={4}>
                 <p className='text-light mt-2'>{t('OS2Contiki - Conference and ticket management')}</p>
               </Col>
-              <Col md={6}>
+              <Col md={4}>
+                {conference.app.accessibility_statement.url &&
+                  <p className='text-light mt-2'><Link to={conference.app.accessibility_statement.url}>{conference.app.accessibility_statement.text}</Link></p>}
+              </Col>
+              <Col md={4}>
                 <Nav variant='pills' defaultActiveKey='/home'>
                   <Nav.Item>
                     <Nav.Link className='disabled text-light'>{t('Select language')}:</Nav.Link>
