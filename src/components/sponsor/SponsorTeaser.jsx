5264b940f298539b4ac848669760eb0ec8834324
Mikkel Ricky <rimi@aarhus.dk>
2020-09-15 16:26:17 +0200

    JOBCAMP-107: Fixed display of main conference sponsors

--- src/components/sponsor/SponsorTeaser.jsx
+++ src/components/sponsor/SponsorTeaser.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import PropTypes from 'prop-types'
 import { useTranslate } from 'react-translate'
@@ -11,19 +12,22 @@ const Sponsor = ({ sponsors }) => {
 
   return (
     <Row className='sponsors scroll-offset-class py-md-5 bg-light'>
-      <Col xs={12} className='text-center'>
-        <h2 className='lead'>{t('Sponsored by')}</h2>
-        {sponsors.map(sponsor => (
-          <Col xs={12} md={sponsors.length >= 4 ? 3 : 12 / sponsors.length} key={sponsor.id} className='mb-3'>
-            <h3 className='h2'>{sponsor.title}</h3>
-            {sponsor.summary ? <p className='summary'>{sponsor.summary}</p> : <ReactHtml html={sponsor.description} />}
-            <Link to={`sponsor/${sponsor.id}`} className='btn btn-primary btn-lg'>
-              {t('Read more about {{sponsor}}', { sponsor: sponsor.title })}
-            </Link>
-          </Col>
-        )
-        )}
-      </Col>
+      <Container>
+        <Col xs={12} className='text-center'>
+          <h2 className='lead'>{t('Sponsored by')}</h2>
+          <Row>
+            {sponsors.map(sponsor => (
+              <Col xs={12} md={sponsors.length >= 4 ? 3 : 12 / sponsors.length} key={sponsor.id} className='mb-3'>
+                <h3 className='h2'>{sponsor.title}</h3>
+                {sponsor.summary ? <p className='summary'>{sponsor.summary}</p> : <ReactHtml html={sponsor.description} />}
+                <Link to={`sponsor/${sponsor.id}`} className='btn btn-primary btn-lg'>
+                  {t('Read more about {{sponsor}}', { sponsor: sponsor.title })}
+                </Link>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Container>
     </Row>
   )
 }
