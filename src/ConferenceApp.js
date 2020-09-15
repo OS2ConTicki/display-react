675c1e7d9162a8996104c912309d597d08c1057e
Mikkel Ricky <rimi@aarhus.dk>
2020-09-15 15:56:54 +0200

    JOBCAMP-105: Fixed navigation scroll

--- src/ConferenceApp.js
+++ src/ConferenceApp.js
@@ -4,11 +4,17 @@ import App from './App'
 import { BrowserRouter, useLocation } from 'react-router-dom'
 
 function ScrollToTop () {
-  const { pathname } = useLocation()
+  const { hash, pathname } = useLocation()
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [pathname])
+    const element = hash && document.querySelector(hash)
+    if (element) {
+      // @see https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+      element.scrollIntoView({ behavior: 'smooth' })
+    } else {
+      window.scrollTo(0, 0)
+    }
+  }, [pathname, hash])
 
   return null
 }
