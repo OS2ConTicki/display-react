ea602c7f16fd774f387334158ba0fa2f7d5b40ff
Mikkel Ricky <rimi@aarhus.dk>
2020-10-01 13:43:11 +0200

    JOBCAMP-116: Made stuff work in IE11

--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,10 @@
+// https://www.npmjs.com/package/react-app-polyfill
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
 import './entry.scss'
 import ConferenceApp from './ConferenceApp'
+
 require('typeface-roboto')
+
 window.ConferenceApp = ConferenceApp
